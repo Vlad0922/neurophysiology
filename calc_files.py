bcbd1d790f879b98b0f865c3321b36a57a75157c
Vlad0922 <vlad0922@yandex.ru>
2018-09-19 15:47:00 +0300

    [clean]
    cleanup debug output

--- calc_files.py
+++ calc_files.py
@@ -98,7 +98,6 @@ def calc_stats(spikes, args):
 
 
 def merge_st(st_list):
-    print(st_list)
     offset = st_list[0][~0];
 
     for i in range(1, len(st_list)):
@@ -109,7 +108,6 @@ def merge_st(st_list):
 
 
 def main(args):
-    print(args.all)
     dist_dir = args.data_dir
     dist_file = '{}.xls'.format(args.dist_file)
 
@@ -141,9 +139,6 @@ def main(args):
                                     for s, d in zip(interval.times, interval.durations):
                                         e = s + d
                                         spikes_filtered = spikes[np.where((spikes >= s) & (spikes <= e))]
-                                        print(s, e, spikes[0], spikes[~0])
-
-                                        print(len(spikes_filtered), spikes_filtered[~0] - spikes_filtered[0])
                                     
                                         if len(spikes_filtered) > 50:
                                             int_spikes.append(spikes_filtered)
